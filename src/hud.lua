bd9a91b666911c95e79f77c0f968589014142249
JEzquerroF <jezquerrof@gmail.com>
2024-07-30 13:26:59 +0200

    Update

--- src/hud.lua
+++ src/hud.lua
@@ -1,6 +1,6 @@
 local Actor  = Actor or require "src/actor"
 local Vector = Vector or require "lib/vector"
-local Hud    = Actor:extend()
+local Hud = Actor:extend()
 
 function Hud:new(aciertos)
     self.aciertos = aciertos or 0
@@ -11,11 +11,24 @@ function Hud:update(dt)
 end
 
 function Hud:draw()
+    --Combo Count
     love.graphics.setFont(self.font)
     love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("fill", 290, 7, 319, 40)
+    love.graphics.rectangle("fill", 300, 7, 200, 40)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.print("Numero de Aciertos: " .. self.aciertos, 300, 17)
+    love.graphics.print("Combo Count: " .. self.aciertos, 305, 17)
+    --Controls
+    love.graphics.setColor(0,0,0)
+    love.graphics.rectangle("fill",690,130,140,40)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("A-D to Aim", 699,135)
+
+    love.graphics.setColor(0,0,0)
+    love.graphics.rectangle("fill",690,170,161,40)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Space to Shoot", 698,175)
+
+  
 end
 
 return Hud
